fe9eeb33f7f2c99f12d440b87209d9c992eaa846
Justin Hong <jjhong922@berkeley.edu>
2020-06-30 20:40:47 -0400

    use h explicitly

---
+++ js/index.js
@@ -0,0 +1,7 @@
+import { h, render } from 'preact';
+import './style';
+import 'preact-material-components/Button/style.css';
+import App from './components/app';
+
+const root = document.getElementById("app");
+render(h(App), root);
--- js/index.jsx
+++
@@ -1,8 +0,0 @@
-const h = require('preact').h;
-const render = require('preact').render;
-require('./style');
-require('preact-material-components/Button/style.css');
-const App = require('./components/app').default;
-
-const root = document.getElementById("app");
-render(<App />, root);
